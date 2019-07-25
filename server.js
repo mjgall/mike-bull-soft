df66ea46f90e155c65bcb6a415feabe50d2d5aa4
Mike Gallagher <mike.gallagh@gmail.com>
2019-07-25 11:58:27 -0500

    no conditional routing

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const path = require('path');
 
 const app = express();
 
-//CONDITIONS IF DEPLOYED TO PRODUCTION
-if (process.env.HTTP_PORT) {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+// //CONDITIONS IF DEPLOYED TO PRODUCTION
+// if (process.env.HTTP_PORT) {
+//   // Express will serve up production assets
+//   // like our main.js file, or main.css file!
+//   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
+//   // Express will serve up the index.html file
+//   // if it doesn't recognize the route
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 //ROUTES
 app.get('/api/theworld', (req, res) => {
