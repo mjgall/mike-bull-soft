edf9138b2c6d2b28e5b93c0bfdcd22b1bd0a6b0b
Mike Gallagher <mike.gallagh@gmail.com>
2019-07-25 11:21:31 -0500

    updated server file

--- server.compiled.js
+++ server.compiled.js
@@ -2,21 +2,20 @@
 
 var express = require('express');
 
-var app = express(); //CONDITIONS IF DEPLOYED TO PRODUCTION
+var app = express(); // //CONDITIONS IF DEPLOYED TO PRODUCTION
+// if (process.env.HTTP_PORT) {
+//   // Express will serve up production assets
+//   // like our main.js file, or main.css file!
+//   app.use(express.static(path.join(__dirname, 'client', 'build')));
+//   // Express will serve up the index.html file
+//   // if it doesn't recognize the route
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
-if (process.env.HTTP_PORT) {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express["static"](path.join(__dirname, 'client', 'build'))); // Express will serve up the index.html file
-  // if it doesn't recognize the route
-
-  var path = require('path');
-
-  app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-} //ROUTES
-
+app.use(express["static"](path.join(__dirname, 'client', 'build'))); //ROUTES
 
 app.get('/api/theworld', function (req, res) {
   res.json({
@@ -27,5 +26,4 @@ app.get('/api/theworld', function (req, res) {
 var port = process.env.HTTP_PORT || 2000;
 app.listen(port, function () {
   console.log("Running on http://localhost:".concat(port));
-  console.log(process.env.HTTP_PORT);
 });
