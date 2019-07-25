922b7107f3e5041278eeaca859b2983bf72d7b20
Mike Gallagher <mike.gallagh@gmail.com>
2019-07-24 23:46:07 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+//CONDITIONS IF DEPLOYED TO PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+//ROUTES
+app.get('/api/theworld', (req, res) => {
+  res.json({ answer: 'worlds' });
+});
+
+//SERVER RUNNING
+const port = process.env.PORT || 2000;
+app.listen(port, () => {
+  console.log(`Running on http://localhost:${port}`);
+});
