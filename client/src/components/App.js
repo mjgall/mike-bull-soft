c4284158089ca71af63edb84821d2ff46b4389d1
Mike Gallagher <mike.gallagh@gmail.com>
2019-07-25 16:53:03 -0500

    Added log in form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment
+} from 'semantic-ui-react';
 
 export default class App extends Component {
   render() {
-    return <Menu>
-      <Menu.Item>MikeBullSoft</Menu.Item>
-    </Menu>;
+    return (
+      <Grid
+        textAlign="center"
+        style={{ height: '100vh' }}
+        verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" textAlign="center">
+            Log In
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="E-mail address"
+              />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+              />
+
+              <Button fluid size="large">
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            New to us? <a href="#">Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
