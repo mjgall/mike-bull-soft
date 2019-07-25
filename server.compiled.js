cee80730cfe0d0f4fc12f43afc6677d7b36d40ea
Mike Gallagher <mike.gallagh@gmail.com>
2019-07-25 10:37:20 -0500

    Build server and add .ebextensions

---
+++ server.compiled.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var express = require('express');
+
+var app = express(); //CONDITIONS IF DEPLOYED TO PRODUCTION
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express["static"](path.join(__dirname, 'client', 'build'))); // Express will serve up the index.html file
+  // if it doesn't recognize the route
+
+  var path = require('path');
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} //ROUTES
+
+
+app.get('/api/theworld', function (req, res) {
+  res.json({
+    answer: 'worlds'
+  });
+}); //SERVER RUNNING
+
+var port = process.env.PORT || 2000;
+app.listen(port, function () {
+  console.log("Running on http://localhost:".concat(port));
+});
