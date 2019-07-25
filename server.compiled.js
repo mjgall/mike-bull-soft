7188cdef2999607387e5332b3abdfa2dc82e32f2
Mike Gallagher <mike.gallagh@gmail.com>
2019-07-25 11:17:57 -0500

    checking for production another way

--- server.compiled.js
+++ server.compiled.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 var app = express(); //CONDITIONS IF DEPLOYED TO PRODUCTION
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.HTTP_PORT) {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express["static"](path.join(__dirname, 'client', 'build'))); // Express will serve up the index.html file
@@ -27,4 +27,5 @@ app.get('/api/theworld', function (req, res) {
 var port = process.env.HTTP_PORT || 2000;
 app.listen(port, function () {
   console.log("Running on http://localhost:".concat(port));
+  console.log(process.env.HTTP_PORT);
 });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 //CONDITIONS IF DEPLOYED TO PRODUCTION
-if (process.env.NODE_ENV === 'production') {
+if (process.env.HTTP_PORT) {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static(path.join(__dirname, 'client', 'build')));
