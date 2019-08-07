f0429c7466b4800e50886dd2e4c7ec1d582f6de5
Mike Gallagher <mike.gallagh@gmail.com>
2019-08-06 22:50:49 -0500

    forgot to compile

--- server.compiled.js
+++ server.compiled.js
@@ -33,18 +33,20 @@ require('./routes/authRoutes')(app); //APP
 require('./routes/appRoutes')(app); //CONDITIONS IF DEPLOYED TO PRODUCTION
 
 
-console.log(process.env); // if (!process.env.TERM_PROGRAM === 'vscode') {
-//   // Express will serve up production assets
-//   // like our main.js file, or main.css file!
-//   app.use(express.static(path.join(__dirname, 'client', 'build')));
-//   // Express will serve up the index.html file
-//   // if it doesn't recognize the route
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+console.log(process.env);
 
-app.use(express.static(path.join(__dirname, 'client', 'build'))); //SERVER RUNNING
+if (process.env.ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static(path.join(__dirname, 'client', 'build'))); // Express will serve up the index.html file
+  // if it doesn't recognize the route
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} // app.use(express.static(path.join(__dirname, 'client', 'build')));
+//SERVER RUNNING
+
 
 const port = process.env.HTTP_PORT || 2000;
 app.listen(port, () => {
