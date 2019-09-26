39791d633dd861e8927a3e1f3ebfb4eb8444c3a7
Mike Gallagher <mike.gallagh@gmail.com>
2019-09-26 17:37:11 -0500

    updated student queries for id and not g_id

--- queries/getAllCourses.js
+++ queries/getAllCourses.js
@@ -7,7 +7,7 @@ module.exports = () => {
         reject(err);
       }
       connection.query(
-        `SELECT courses.id AS id, users.id AS user_id, users.google_id, users.first_name, users.last_name, courses.title, courses.create_date FROM courses JOIN users ON courses.owner_id WHERE courses.owner_id = users.google_id;`,
+        `SELECT courses.id AS id, users.id AS user_id, users.first_name, users.last_name, courses.title, courses.create_date FROM courses JOIN users ON courses.owner_id WHERE courses.owner_id = users.id;`,
         (err, courses, fields) => {
           if (err) {
             reject(err);
