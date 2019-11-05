1ef55438fdb7bcac4c98332e9e6d7dcc4cf25b71
Mike Gallagher <mike.gallagh@gmail.com>
2019-11-04 22:25:55 -0600

    accidental previous commit

---
+++ client/build/precache-manifest.f6cb44dd38fb71c24f0065ba2ca6d2f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a40049ae52985af9a85fbad339cb4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "335418cbd528e326a9cb",
+    "url": "/static/css/main.50a98029.chunk.css"
+  },
+  {
+    "revision": "ab181446bbdfd9944dd4",
+    "url": "/static/js/2.f0ae6db6.chunk.js"
+  },
+  {
+    "revision": "335418cbd528e326a9cb",
+    "url": "/static/js/main.fc0969df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
