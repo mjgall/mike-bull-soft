14c5190a2306a32a130492b88d29fc6fa61c3308
Mike Gallagher <mike.gallagh@gmail.com>
2019-12-07 18:43:12 -0600

    menu layout for lessons

--- client/src/components/Student/StudentLesson.js
+++ client/src/components/Student/StudentLesson.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Pane, Text, Heading, Card, Icon } from 'evergreen-ui';
+// import { Heading, Card, Icon } from 'evergreen-ui';
+import { Tab, Icon } from 'semantic-ui-react';
+import Loader from '../Loader';
 import * as utils from '../../utils';
 
 export default class StudentLesson extends React.Component {
@@ -13,24 +15,38 @@ export default class StudentLesson extends React.Component {
 
   componentDidMount = async () => {
     const lesson = await utils.fetchLesson(this.state.lessonId);
-    const symbols = await utils.fetchSymbols(this.state.courseId)
+    const symbols = await utils.fetchSymbols(this.state.courseId);
+
     this.setState({ lesson: lesson.response, symbols });
+    const panes = this.state.symbols.map(symbol => {
+      return {
+        // menuItem: () => <div><Icon name="dot circle"></Icon>{symbol.id}</div>,
+        menuItem: symbol.id,
+        render: () => <Tab.Pane>{symbol.text}</Tab.Pane>
+      };
+    });
+    this.setState({ panes });
     this.setState({ loaded: true });
   };
 
+  panes = [
+    { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+    { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+    { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> }
+  ];
+
   render() {
-    console.log(this.state.courseId, this.state.lessonId);
+    console.log(this.state);
     return this.state.loaded ? (
-      <Card
-        height="10rem"
-        width="100%"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        border="default">
-        <Heading>{this.state.lesson.title}</Heading>
-        <Icon icon="full-circle" color="green"></Icon>
-      </Card>
-    ) : null;
+      <Tab
+        style={{ width: '100%' }}
+        menu={{ fluid: true, vertical: true }}
+        menuPosition="right"
+        panes={this.state.panes}
+      />
+    ) : (
+      // <h1>Hello</h1>
+      <Loader></Loader>
+    );
   }
 }
