55e4aaabdc56ca5b2d15a09239d501b8fd26ff8f
Mike Gallagher <mike.gallagh@gmail.com>
2020-01-07 18:27:24 -0600

    module export system

--- queries/deleteUser.js
+++ queries/deleteUser.js
@@ -3,7 +3,7 @@ const sqlString = require('sqlstring');
 
 //SHOULD USE PROMISES AND NOT A CALLBACK
 
-export default user_id => {
+module.exports = user_id => {
   return new Promise((resolve, reject) => {
     db.getConnection((err, connection) => {
       if (err) throw err;
