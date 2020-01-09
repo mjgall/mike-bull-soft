dcfb3de5d314f8ab6eb31d2cd13bcdc54e30812d
Mike Gallagher <mike.gallagh@gmail.com>
2020-01-09 08:23:49 -0600

    too early on last commit

---
+++ client/build/precache-manifest.91ba53c2825350f7eda1a7f4d1672261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadb212d86e99eec0c7bff04c2177852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e594fe48108147d822cb",
+    "url": "/static/css/main.170188d0.chunk.css"
+  },
+  {
+    "revision": "9742d51524deeb636392",
+    "url": "/static/js/2.5c18c492.chunk.js"
+  },
+  {
+    "revision": "e594fe48108147d822cb",
+    "url": "/static/js/main.74ddc4fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
