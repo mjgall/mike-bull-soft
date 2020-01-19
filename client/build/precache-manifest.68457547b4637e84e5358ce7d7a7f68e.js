50bd0989e218b61ed705ed4b1c5676f44f28d51f
Mike Gallagher <mike.gallagh@gmail.com>
2020-01-18 18:27:02 -0600

    1.5 second timeout after reset for redirect

---
+++ client/build/precache-manifest.68457547b4637e84e5358ce7d7a7f68e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656512077e4772a51ee5fd5ff5ec3521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d3117afec1e7c8f6c2e",
+    "url": "/static/css/main.2c363501.chunk.css"
+  },
+  {
+    "revision": "de3d15037cbd084ea47e",
+    "url": "/static/js/2.514be03d.chunk.js"
+  },
+  {
+    "revision": "34a738d6424da09f3c5f21f93853a2b9",
+    "url": "/static/js/2.514be03d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d3117afec1e7c8f6c2e",
+    "url": "/static/js/main.71cc5972.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
