bddb98142162d8e42b8a1b8b678c3d611b502727
Mike Gallagher <mike.gallagh@gmail.com>
2020-03-02 13:32:10 -0600

    process.envv typo

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  cookieKey: process.envv.COOKIE_KEY,
+  cookieKey: process.env.COOKIE_KEY,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.REGION,
